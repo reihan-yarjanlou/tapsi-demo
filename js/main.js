1fd55f7429703eb6d97a554ead913a73cea8ea2f
saheb mohammadi <saheb.put92@gmail.com>
2021-10-23 00:39:46 +0330

    add new sections

--- js/main.js
+++ js/main.js
@@ -19,3 +19,12 @@ tabs.forEach((tab) => {
     targeTabContent.classList.add("active");
   });
 });
+
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+const accordion = document.querySelector(".accordion");
+accordionHeaders.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    console.log(e.target.parentElement);
+    e.target.parentElement.classList.toggle("accordion__expanded");
+  });
+});
